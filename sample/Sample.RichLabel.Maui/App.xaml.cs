66b2288e49229b70c4321036674c8853f72362b3
jvelarde <jvicius@gmail.com>
2026-07-03 16:01:37 -0600

    Add README.md, Add Handling Actions Add JSON Data Example New Release

--- sample/Sample.RichLabel.Maui/App.xaml.cs
+++ sample/Sample.RichLabel.Maui/App.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace Sample.RichLabel.Maui
+﻿using RichLabel.Maui.Handlers;
+
+namespace Sample.RichLabel.Maui
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            InitActionTouchHandler();
+        }
+
+        public void InitActionTouchHandler()
+        {
+            RichActionHandler.Register("phone", async value =>
+            {
+                await Shell.Current.DisplayAlert("Phone Data", $"Parameter value: {value}", "Close");
+            });
+            RichActionHandler.Register("text", async value =>
+            {
+                await Shell.Current.DisplayAlert("Text Data", $"Parameter value: {value}", "Close");
+            });
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
